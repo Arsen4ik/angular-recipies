bc8ce1367065ef45790a1d7ac8f124358b46c868
Arsen4ik <arsenfazleev@gmail.com>
2024-04-17 14:00:55 +0300

    commit 1: app is ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RecipesService } from './services/recipes.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'myapp';
+  title = 'recipes';
+  recipes = inject(RecipesService)
+  constructor() {
+    this.recipes.fetchRecipes()
+  }
 }
